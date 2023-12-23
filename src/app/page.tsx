a4e8aa793fdbcf4be440892b0ebdf3ac3196230e
HyeongJin Park <hywlss9@gmail.com>
2023-12-23 17:45:33 +0900

    refactor: ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Button from '@/components/button';
+import Card from '@/components/card';
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div>test</div>
+    <main className='flex flex-col items-center w-[500px] h-[400px] p-4'>
+      <Button>Add New Note</Button>
+      <ul className='flex flex-col gap-y-4 overflow-auto w-full py-4'>
+        <li>
+          <Card />
+        </li>
+        <li>
+          <Card />
+        </li>
+        <li>
+          <Card />
+        </li>
+      </ul>
     </main>
   );
 }
